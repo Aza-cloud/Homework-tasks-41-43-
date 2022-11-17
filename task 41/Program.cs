504c4e9b008ff4ef30106a398d7b649458f0bd6c
Orange <dunduk201@yandex.ru>
2022-11-17 13:49:20 +0900

    Added one file

---
+++ task 41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.Write("Через пробел введите элементы: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int result = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        result++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {result}");
